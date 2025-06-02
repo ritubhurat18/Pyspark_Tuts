6326a08f22816e223e4965a987b4292c5534ced0
ritubhurat18 <ritubhurat18>
2025-06-02 17:40:28 +0000

    initial commit

---
+++ src/code/Word_Count.py
@@ -0,0 +1,34 @@
+# Databricks notebook source
+
+# Pyspark Word Count Program 
+from pyspark.sql import SparkSession
+from pyspark.sql.types import *
+from pyspark.sql import functions as F
+
+# COMMAND ----------
+
+
+spark = SparkSession.builder.appName("Pyspark Word Count").getOrCreate()
+txt = "Hello hello Reetu, Here's your First@ Word Count Program in Spark for count word!"
+df = spark.createDataFrame([txt], StringType())
+
+
+def clean_text(df):
+    clean_df = df.\
+        select(F.lower(F.regexp_replace(df.value,"[^A-Za-z0-9\\s\'$]",""))\
+        .alias("cleaned_value"))
+    return clean_df
+
+def word_count(df):
+    split_df = df.\
+        select(F.split(F.col("cleaned_value")," ").alias("splitted_value")).\
+        select(F.explode(F.col("splitted_value")).alias("word"))
+    word_count = split_df.\
+        groupBy(split_df.word).agg(F.count(split_df.word).alias("count"))
+    return word_count
+
+clean_df = clean_text(df)
+clean_df.show(truncate=False)
+word_count_df = word_count(clean_df)
+word_count_df.show(truncate=False)
+
---
+++ test/Test_Word_Count.py
@@ -0,0 +1,7 @@
+# Databricks notebook source
+# MAGIC %pip install pytest
+# MAGIC %pip install pytest mock
+
+# COMMAND ----------
+
+# MAGIC %restart_python
